35214a4b72e3eab99bcd3c441e0d751820192f0b
paulstansifer <paulstansifer@users.noreply.github.com>
2025-11-21 17:38:43 +0000

    deploy: 541bcf58cbcdb97cce68bc848150ff47cf01bd5f

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/mosaic-disco-urn/',
-  plugins: [react()],
-})
